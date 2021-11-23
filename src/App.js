6375e6aa8705ab233474a6500bc17bf27ced26ac
Mprabhak <Manoj.prabhakars@analog.com>
2021-11-23 10:48:23 +0530

    iitial template commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import "./App.css";
+import "./components/Header";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Box } from "@mui/material";
+
+function App() {
+  return (
+    <Box
+      sx={{
+        display: "grid",
+        gridTemplateRows: "1fr 1fr 5fr 1fr",
+        height: "100vh",
+        bgcolor: "#e79f4cef",
+      }}
+    >
+      <Header />
+      <h1>Quiz App</h1>
+      <Footer />
+    </Box>
+  );
+}
+
+export default App;
